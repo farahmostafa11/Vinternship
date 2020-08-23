bc63aa03f1bc5f2bf8c454a66773b4546cbf0c18
farahmostafa11 <58088212+farahmostafa11@users.noreply.github.com>
2020-08-23 12:41:14 +0200

    add new file

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,4 @@
-const { htmlPrefilter } = require("jquery");
+//const { htmlPrefilter } = require("jquery");
 
 console.log('\'Allo \'Allo!');
 
@@ -10,4 +10,50 @@ console.log('\'Allo \'Allo!');
 // https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
 // $(function () { $('[data-toggle="popover"]').popover(); });
 
+let faq1=document.getElementsByClassName('search-icon1')[0];
+let faq2=document.getElementsByClassName('search-icon2')[0];
+let faq3=document.getElementsByClassName('search-icon3')[0];
+console.log(faq1);
+console.log(faq2);
+console.log(faq3);
 
+faq1.addEventListener('click',arrowtoggel);
+function arrowtoggel(){
+    faq1.classList.toggle('flaticon-down-chevron');
+    faq1.classList.toggle('flaticon-up-chevron');
+    if(faq2.classList.contains('flaticon-up-chevron')){
+        faq2.classList.toggle('flaticon-down-chevron');
+        faq2.classList.toggle('flaticon-up-chevron');
+    }
+    if(faq3.classList.contains('flaticon-up-chevron')){
+        faq3.classList.toggle('flaticon-down-chevron');
+        faq3.classList.toggle('flaticon-up-chevron');
+    }
+}
+
+faq2.addEventListener('click',arrowtoggel2);
+function arrowtoggel2(){
+    faq2.classList.toggle('flaticon-down-chevron');
+    faq2.classList.toggle('flaticon-up-chevron');
+    if(faq1.classList.contains('flaticon-up-chevron')){
+        faq1.classList.toggle('flaticon-down-chevron');
+        faq1.classList.toggle('flaticon-up-chevron');
+    }
+    if(faq3.classList.contains('flaticon-up-chevron')){
+        faq3.classList.toggle('flaticon-down-chevron');
+        faq3.classList.toggle('flaticon-up-chevron');
+    }
+}
+faq3.addEventListener('click',arrowtoggel3);
+function arrowtoggel3(){
+    faq3.classList.toggle('flaticon-down-chevron');
+    faq3.classList.toggle('flaticon-up-chevron');
+    if(faq2.classList.contains('flaticon-up-chevron')){
+        faq2.classList.toggle('flaticon-down-chevron');
+        faq2.classList.toggle('flaticon-up-chevron');
+    }
+    if(faq1.classList.contains('flaticon-up-chevron')){
+        faq1.classList.toggle('flaticon-down-chevron');
+        faq1.classList.toggle('flaticon-up-chevron');
+    }
+}
