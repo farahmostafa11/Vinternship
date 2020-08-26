794ea60b301f1afda2cf00d577ac05a604baaff6
farahmostafa11 <58088212+farahmostafa11@users.noreply.github.com>
2020-08-26 12:11:28 +0200

    add new file

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,12 +13,64 @@ console.log('\'Allo \'Allo!');
 let faq1 = document.getElementsByClassName('search-icon1')[0];
 let faq2 = document.getElementsByClassName('search-icon2')[0];
 let faq3 = document.getElementsByClassName('search-icon3')[0];
+let cash = document.getElementsByClassName('circle')[0];
+let visa = document.getElementsByClassName('circle')[1];
+let closemodal = document.getElementsByClassName('close-modal')[0];
+let close_address_modal = document.getElementsByClassName('close-modal')[1];
+let checkmodal = document.getElementsByClassName('modal-container')[0];
+let new_address = document.getElementsByClassName('sel-btn')[0];
+let check_out_btn = document.getElementsByClassName('sel-btn')[1];
+let check_modal = document.getElementsByClassName('modal-container')[0];
+let address_modal = document.getElementsByClassName('modal-container')[1];
 let text1 = document.getElementById('myinput');
 console.log(text1);
 console.log(faq1);
 console.log(faq2);
 console.log(faq3);
+console.log(cash);
+console.log(visa);
+console.log(check_out_btn);
+console.log(cash.style.backgroundColor);
+console.log(visa.style.backgroundColor);
 
+closemodal.addEventListener('click',close_modal);
+function close_modal(){
+  checkmodal.classList.remove('show');
+}
+
+close_address_modal.addEventListener('click',closeaddress_modal);
+function closeaddress_modal(){
+  address_modal.classList.remove('show');
+}
+
+new_address.addEventListener('click',show_address_modal)
+
+function show_address_modal(){
+  address_modal.classList.add('show');
+}
+
+check_out_btn.addEventListener('click',show_right_modal)
+
+function show_right_modal(){
+  check_modal.classList.add('show');
+}
+
+
+cash.addEventListener('click',greentowhite);
+
+function greentowhite(){
+    cash.style.backgroundColor= '#18A232';
+    visa.style.backgroundColor= 'white';
+}
+
+visa.addEventListener('click',whitetogreen);
+
+function whitetogreen(){
+  if(visa.style.backgroundColor === 'white'){
+    cash.style.backgroundColor= 'white';
+    visa.style.backgroundColor= '#18A232';
+  }
+}
 faq1.addEventListener('click', arrowtoggel);
 
 function arrowtoggel() {
@@ -79,3 +131,8 @@ text1.addEventListener('input', function () {
     Results.classList.remove('none');
   }
 });
+
+
+
+
+
