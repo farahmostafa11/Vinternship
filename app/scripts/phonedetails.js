aac83096e37b73c1fc785b5ae730a6216012319b
unknown <farahmostafa459@gmail.com>
2020-09-02 10:23:36 +0200

    add new file

--- app/scripts/phonedetails.js
+++ app/scripts/phonedetails.js
@@ -1,4 +1,5 @@
 let lastselectedcolor = document.getElementsByClassName('circle')[0];
+let lastselectedgega = document.getElementsByClassName('circle')[4];
 
 for (let i = 0; i < 4; i++) {
     let test=document.getElementsByClassName('circle')[i];
@@ -9,6 +10,17 @@ function changeselectedcolor(){
     lastselectedcolor.classList.remove('active-color');
     this.classList.add('active-color');
     lastselectedcolor=this;
+}
+
+for (let i = 4; i < 7; i++) {
+    let test1=document.getElementsByClassName('circle')[i];
+    test1.addEventListener('click',changeselectedgega);
+}
+
+function changeselectedgega(){
+    lastselectedgega.classList.remove('active-gega');
+    this.classList.add('active-gega');
+    lastselectedgega=this;
 }
 
 let lastselectedphoto = document.getElementsByClassName('mini')[0];
